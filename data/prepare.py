c8fdc149dd46661c19a6751d7497d28e73e7c1e7
rsymbol <retrosymbol@gmail.com>
2018-06-18 18:02:19 +0300

    version 20180618

--- data/prepare.py
+++ data/prepare.py
@@ -5,6 +5,7 @@ import seaborn as sns
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import LabelBinarizer
+from collections import defaultdict
 
 from scipy import stats
 
@@ -17,7 +18,7 @@ def new_dict(lst):
 # load data
 db = util.get_db()
 
-#-------------------------------INFORMATION----------------------------------------------------
+# -------------------------------INFORMATION----------------------------------------------------
 
 # common info
 print(db.info())
@@ -30,40 +31,29 @@ print('Unique values of columns: {}'.format({c: len(db[c].unique()) for c in db.
 print('---------------------------------')
 
 # unique values of specified columns
-print(db['Pclass'].value_counts())
+print(db['Embarked'].value_counts())
 print('---------------------------------')
 
+# -------------------------------TRANSFORMATION----------------------------------------------------
+db_copy = db.copy().fillna('0')
+# preprocessing categorical data
+cat_val = ['Pclass', 'Sex', 'Embarked']
 
-#-------------------------------TRANSFORMATION----------------------------------------------------
-
-db_copy = db.copy()
-
-lb_make = LabelEncoder()
-db_copy["Sex_"] = lb_make.fit_transform(db_copy["Sex"])
-print(db_copy.head())
-
-# lb_style = LabelBinarizer()
-# lb_results = lb_style.fit_transform(db_copy["Sex"])
+lb = {k: LabelBinarizer().fit(db_copy[k]) for k in cat_val}
+print(lb.get(1))
+# lb = LabelBinarizer()
+# lb_results = lb.fit_transform(db_copy["Sex"])
 # print(pd.DataFrame(lb_results, columns=lb_style.classes_).head())
 
 
-
-
-
-
-
-
-
-
 # ????
-print(new_dict(db['Sex']))
+# print(new_dict(db['Sex']))
 
 # EXAMPLE replacing some item to needs
 # cleanup_nums = {"num_doors": {"four": 4, "two": 2},
 #                 "num_cylinders": {"four": 4, "six": 6, "five": 5, "eight": 8,
 #                                   "two": 2, "twelve": 12, "three": 3}}
 # db.replace(cleanup_nums, inplace=True)
-
 
 
 # corr = np.corrcoef(db, rowvar=False)
@@ -75,3 +65,28 @@ print(new_dict(db['Sex']))
 
 # print(stats.describe(db))
 # print('------------------------------------------------------')
+
+
+#
+#
+# d = defaultdict(LabelEncoder)
+# print(type(d))
+#
+# # Encoding the variable
+# fit = db_copy.apply(lambda x: d[x.name].fit_transform(x))
+# print(fit.shape)
+#
+# # Inverse the encoded
+# fit.apply(lambda x: d[x.name].inverse_transform(x))
+#
+#
+# # Using the dictionary to label future data
+# db_copy.apply(lambda x: d[x.name].transform(x))
+#
+#
+# cat_val = ['Pclass', 'Sex': 2, 'Age': 89, 'SibSp': 7, 'Parch': 7, 'Ticket': 681, 'Fare': 248, 'Cabin': 148, 'Embarked': 4, 'Survived': 2]
+#
+#
+# lb_make = LabelEncoder()
+# db_copy["Sex_"] = lb_make.fit_transform(db_copy["Sex"])
+# print(db_copy.head())
