1e4009f9f2d429e1a72cf01e928580361b5d91ab
sol <retrosymbol@gmail.com>
2018-06-27 00:03:54 +0300

    20180627

--- challenge_data/load_cd.py
+++ challenge_data/load_cd.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import util
 
-db = pd.read_csv('challenge_data/train_data_prepared.zip', index_col=False, compression='zip', delimiter=';')
+db = pd.read_csv('../challenge_data/train_data_prepared.zip', index_col=False, compression='zip', delimiter=';')
 print(db.columns)
 
 # define basic terms
@@ -12,10 +12,11 @@ data = X.assign(target_column=y).rename(columns={"target_column": target_column}
 feature_names = data.columns.values
 
 # save prepared data to csv
-X.to_csv("data/csv/X.csv", header=False, index=False)
-y.to_csv("data/csv/y.csv", header=False, index=False)
-pd.DataFrame(feature_names).to_csv("data/csv/feature_names.csv", header=False, index=False)
+X.to_csv("../data/csv/X.csv", header=False, index=False)
+y.to_csv("../data/csv/y.csv", header=False, index=False)
+pd.DataFrame(feature_names).to_csv("../data/csv/feature_names.csv", header=False, index=False)
 util.__save_obj('data', data)
+util.__save_obj('data_copy', data)
 
 # print summary info
 print("-----------------------------")
